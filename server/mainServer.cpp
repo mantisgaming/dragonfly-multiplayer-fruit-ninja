756dab846168ea02c9c3862ecccd87c8ece63be2
mantis <mjbgtaad56@gmail.com>
2024-09-17 12:47:23 -0400

    Added dragonfly code to sample

--- server/mainServer.cpp
+++ server/mainServer.cpp
@@ -1,9 +1,24 @@
 
-#include <iostream>
+#include <GameManager.h>
+#include <LogManager.h>
+
 #include "ServerSample.h"
 #include <SharedSample.h>
 
 int main(int argc, char** argv) {
-	std::cout << ServerSample::serverString << std::endl;
-	std::cout << SharedSample::sharedString << std::endl;
+    // Start up game manager.
+    if (GM.startUp()) {
+        LM.writeLog("Error starting game manager!");
+        GM.shutDown();
+        return 0;
+    }
+
+    LM.writeLog(ServerSample::serverString.c_str());
+    LM.writeLog(SharedSample::sharedString.c_str());
+
+    // Show splash screen.
+    df::splash();
+
+    // Shut everything down.
+    GM.shutDown();
 }
