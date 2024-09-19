cfd89f9d52cef4cdb9f28ead86e845dae85a8825
mantis <mjbgtaad56@gmail.com>
2024-09-19 13:02:10 -0400

    Started fruit

---
+++ shared/Fruit.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Object.h>
+#include <EventOut.h>
+#include <EventCollision.h>
+
+const int NUM_FRUITS = 5;
+const std::string FRUIT[NUM_FRUITS] = {
+  "pear",
+  "grapes",
+  "apple",
+  "banana",
+  "blueberries"
+};
+
+const std::string FRUIT_STRING = "Fruit";
+const uint8_t FRUIT_OBJET_ID = 1;
+
+const int EXPLOSION_AGE = 45;        // in ticks
+const float EXPLOSION_SPEED = 0.05f; // in spaces/tick
+const float EXPLOSION_ROTATE = 1.0f; // in degrees
+
+class Fruit : df::Object {
+private:
+	void explode();
+public:
+	Fruit(std::string& sprite);
+	int eventHandler(const df::Event* p_e) override;
+	int out(const df::EventOut* p_e);
+	int collide(const df::EventCollision* p_e);
+	bool isExiting();
+	void start(float speed);
+};
+
